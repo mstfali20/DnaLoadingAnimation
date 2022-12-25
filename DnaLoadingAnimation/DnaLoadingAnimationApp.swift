9fb331a19d8d8e4d48af5eeeb43940ebbf91da59
Mustafa Ali KILCI <mustafa_klc997@hotmail.com>
2022-12-25 23:51:30 +0300

    Initial Commit

---
+++ DnaLoadingAnimation/DnaLoadingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DnaLoadingAnimationApp.swift
+//  DnaLoadingAnimation
+//
+//  Created by Mustafa Ali KILCI on 25.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct DnaLoadingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
